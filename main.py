791291021869bf2f666c8e109c3fce378d4c7f29
khanhphamj <phukhanh1903@gmail.com>
2025-02-28 23:12:01 +0700

    build 11: add readme.md

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from core.chatbot.assistant import start_chat
+from core.chatbot.assistant import Assistant
 
+def main():
+    assistant = Assistant()
+    assistant.run()
 
 if __name__ == "__main__":
-    # Bắt đầu cuộc trò chuyện với chatbot
-    chat = start_chat()
-    print("Chatbot ready to assist customers!")
+    main()
